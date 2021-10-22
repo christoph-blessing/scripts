d2e6f635e2877aa2cd774f052c9f6644c0e43616
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-10-22 15:19:36 +0200

    Pipe accounts into dmenu

--- oath_code.sh
+++ oath_code.sh
@@ -7,9 +7,6 @@ if [ $(ykman list | wc -l) -eq 0 ]; then
     exit 1
 fi
 
-account=$(echo '' | dmenu -p "Account:" -fn '-xos4-terminus-medium-r-*-*-14-*' -b)
-code=$(ykman oath accounts code --single "$account" 2> /dev/null)  || { dunstify \
-    --hints='string:x-dunst-stack-tag:oath_code' \
-    --appname='oath_code.sh' \
-    "No account with name '$account'"; exit 1; }
+account=$(ykman oath accounts list | dmenu)
+code=$(ykman oath accounts code --single "$account" 2> /dev/null)
 echo "$code" | xclip -selection c
