738c9162cd72dfd80dd07b522beaf3e5020bb096
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-10-22 15:11:19 +0200

    Add error notification if no yubikey was found

--- oath_code.sh
+++ oath_code.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ $(ykman list | wc -l) -eq 0 ]; then
+    dunstify --hints='string:x-dunst-stack-tag:oath_code' \
+        --appname='oath_code.sh' \
+        'No yubikey found'
+    exit 1
+fi
+
 account=$(echo '' | dmenu -p "Account:" -fn '-xos4-terminus-medium-r-*-*-14-*' -b)
 code=$(ykman oath accounts code --single "$account" 2> /dev/null)  || { dunstify \
     --hints='string:x-dunst-stack-tag:oath_code' \
