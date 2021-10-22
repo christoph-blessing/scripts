e537e92190897a16b6482db69e029ab465ca9082
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-10-22 15:00:57 +0200

    Add script for syncing mail

---
+++ sync_mail.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mbsync --config ~/.config/mbsync/mbsyncrc --all --quiet
+
+prefix="$XDG_DATA_HOME/mail/"
+
+for acc in "$prefix"*; do
+    new_count=$(find  "$acc"/inbox/new -type f -newer ~/.config/neomutt/last-mail-sync 2> /dev/null | wc -l)
+    if [ "$new_count" -gt "0" ]; then
+        dunstify --hints string:x-dunst-stack-tag:sync_mail "New Mail (${acc#"$prefix"})"
+    fi
+done
+
+touch ~/.config/neomutt/last-mail-sync
