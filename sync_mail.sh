85e3a6ad65d247ef2dd87d4c6d40ef4b5029d496
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2021-12-28 17:03:48 +0100

    Add action to new mail notification

--- sync_mail.sh
+++ sync_mail.sh
@@ -1,13 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 
 mbsync --config ~/.config/mbsync/mbsyncrc --all
 
 prefix="$XDG_DATA_HOME/mail/"
 
-for acc in "$prefix"*; do
-    new_count=$(find  "$acc"/inbox/new -type f -newer ~/.config/neomutt/last-mail-sync 2> /dev/null | wc -l)
-    if [ "$new_count" -gt "0" ]; then
-        dunstify --hints string:x-dunst-stack-tag:sync_mail "New Mail (${acc#"$prefix"})"
+for acc_path in "$prefix"*; do
+    new_count=$(find  "$acc_path"/inbox/new -type f -newer ~/.config/neomutt/last-mail-sync 2> /dev/null | wc -l)
+    if [ "$new_count" -gt '0' ]; then
+        acc_name="${acc_path#"$prefix"}"
+        action=$(dunstify --action open,'Open Mutt' --hints string:x-dunst-stack-tag:new_mail_"$acc_name" "New Mail ($acc_name)")
+        if [ "$action" = 'open' ]; then
+            st -e neomutt -f +"$acc_name"/inbox
+        fi
     fi
 done
 
