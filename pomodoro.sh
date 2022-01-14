19af081fb3fbecdfa724f5b8a766bfb79fde1306
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-01-14 13:48:31 +0100

    Keep track of how many pomodoros have passed

--- pomodoro.sh
+++ pomodoro.sh
@@ -1,8 +1,31 @@
 #!/bin/sh
 
 dur=$(($1 * 60))
+long_break_count=4
+tmpdir=/tmp/pomodoro
+
+if ! [ -d "$tmpdir" ]; then
+    mkdir "$tmpdir"
+fi
+
+if [ -e "$tmpdir"/count ]; then
+    old_count=$(cat "$tmpdir"/count)
+    new_count=$((old_count + 1))
+    echo "$new_count" > "$tmpdir"/count
+else
+    echo 1 > "$tmpdir"/count
+fi
+
+touch "$tmpdir"/is_running
+trap 'rm -f -- "$tmpdir"/is_running' EXIT
 
 dunstctl set-paused true
 sleep "$dur"
 dunstctl set-paused false
-dunstify --hints string:x-dunst-stack-tag:pomodoro_finished "Take a break"
+
+if [ "$new_count" = "$long_break_count" ]; then
+    dunstify --hints string:x-dunst-stack-tag:pomodoro_finished "Take a long break"
+    rm "$tmpdir"/count
+else
+    dunstify --hints string:x-dunst-stack-tag:pomodoro_finished "Take a break"
+fi
