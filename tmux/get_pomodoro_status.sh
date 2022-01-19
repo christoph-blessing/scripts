ab9592c71eb0fa529b156d3299f452cb872276ab
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-01-19 16:50:31 +0100

    Change filename in get pomodoro status

--- tmux/get_pomodoro_status.sh
+++ tmux/get_pomodoro_status.sh
@@ -3,7 +3,7 @@
 tmpdir=/tmp/pomodoro
 
 if [ -e "$tmpdir"/is_running ]; then
-    count=$(cat "$tmpdir"/count)
+    count=$(cat "$tmpdir"/current_count)
     long_break_count=$(cat "$tmpdir"/long_break_count)
     echo "$count"/"$long_break_count 🍅"
 fi
