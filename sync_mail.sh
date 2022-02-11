c434baa9dbf5d05a90b9e9d8e618445d1f10b24e
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-02-11 11:01:39 +0100

    Use /home/chris/.local/share/mail in sync_mail.sh

--- sync_mail.sh
+++ sync_mail.sh
@@ -7,12 +7,10 @@ if [ "$1" = '--no-notify' ]; then
     exit
 fi
 
-prefix="$XDG_DATA_HOME/mail/"
-
-for acc_path in "$prefix"*; do
+for acc_path in "$MAILDIR"*; do
     new_count=$(find  "$acc_path"/inbox/new -type f -newer ~/.config/neomutt/last-mail-sync 2> /dev/null | wc -l)
     if [ "$new_count" -gt '0' ]; then
-        acc_name="${acc_path#"$prefix"}"
+        acc_name="${acc_path#"$MAILDIR"}"
         action=$(dunstify --action open,'Open Mutt' --hints string:x-dunst-stack-tag:new_mail_"$acc_name" "New Mail ($acc_name)")
         if [ "$action" = 'open' ]; then
             st -e neomutt -f +"$acc_name"/inbox
