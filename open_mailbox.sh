2a29d413c7ef014f89b9b99c73b91ab8909d3969
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-02-16 16:48:21 +0100

    Fix mailbox opening on ESC

--- open_mailbox.sh
+++ open_mailbox.sh
@@ -3,6 +3,10 @@
 mailboxes=$(fd --type d --maxdepth 2 . "$MAILDIR")
 mailbox=$(echo "$mailboxes" | dmenu)
 
+if [ -z "$mailbox" ]; then
+    exit
+fi
+
 if [ "$(dirname "$mailbox")" = "$MAILDIR" ]; then 
     st -e neomutt -f "$mailbox"/inbox
 else
