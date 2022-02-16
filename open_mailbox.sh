07624ad5e3cc77865985df7e42b281085f63b6f4
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-02-16 17:07:12 +0100

    Add improved open mailbox selection menu
    * Display <account>/<mailbox> instead of full path
    * Only display <account> instead of <account> and <account>/inbox

--- open_mailbox.sh
+++ open_mailbox.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
-mailboxes=$(fd --type d --maxdepth 2 . "$MAILDIR")
+mailboxes=$(fd --base-directory "$MAILDIR" --strip-cwd-prefix --exclude '*/inbox/' --type d --maxdepth 2 .)
 mailbox=$(echo "$mailboxes" | dmenu)
 
 if [ -z "$mailbox" ]; then
     exit
 fi
 
-if [ "$(dirname "$mailbox")" = "$MAILDIR" ]; then 
-    st -e neomutt -f "$mailbox"/inbox
+if test "${mailbox#*/}" != "$mailbox"; then 
+    st -e neomutt -f +"$mailbox"
 else
-    st -e neomutt -f "$mailbox"
+    st -e neomutt -f +"$mailbox"/inbox
 fi
