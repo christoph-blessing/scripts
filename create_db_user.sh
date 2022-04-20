51ec2cf79f070dadad0748042c6d7451c79f10de
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-04-20 10:47:45 +0200

    Add updated script for creating db users
    Improve formatting of message that is copied to clipboard, add link to
    Datajoint page that explains how to connect.

--- create_db_user.sh
+++ create_db_user.sh
@@ -9,4 +9,14 @@ ssh "$db_server" "mysql --execute \"CREATE USER '$user'@'%' IDENTIFIED BY '$pass
 GRANT ALL PRIVILEGES ON \\\`$user\\_%\\\`.* TO '$user'@'%';\n
 GRANT ALL PRIVILEGES ON \\\`nnfabrik\\_$user\\_%\\\`.* TO '$user'@'%';\"" > /dev/null
 
-echo "Username: '$user'; Password: '$password'" | xclip -selection c
+xclip -selection c << EOF
+Database Server Credentials:
+host: '$db_server'
+user: '$user'
+password: '$password'
+
+How to connect:
+https://docs.datajoint.org/python/setup/01-Install-and-Connect.html
+
+You have full privileges on schemas that start with '${user}_'.
+EOF
