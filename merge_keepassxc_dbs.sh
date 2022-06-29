d601436309cd60c5753ad77067ee17067a678bee
Christoph Blessing <33834216+cblessing24@users.noreply.github.com>
2022-06-29 09:57:32 +0200

    Add mounting logic to keepass merging script
    - Mounts the device if not already mounted
    - Unmounts on exit if not originally mounted

--- merge_keepassxc_dbs.sh
+++ merge_keepassxc_dbs.sh
@@ -4,5 +4,7 @@ set -e
 local_db="$XDG_DATA_HOME"/keepassxc/Passwords.kdbx
 android_db="$HOME"/mnt/Internal\ shared\ storage/Documents/Passwords.kdbx
 
+mountpoint "$HOME"/mnt || (mount.jmtpfs "$HOME"/mnt && trap 'umount $HOME/mnt' EXIT)
+
 keepassxc-cli merge --yubikey 2 --same-credentials "$local_db" "$android_db"
 cp "$local_db" "$android_db"
